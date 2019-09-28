6226188c17fe9e6dc86b920346367f172a2d65da
FaizMamat <faiz_mamat@dell.com>
2019-09-29 00:17:04 +0800

    First Commit

---
+++ src/routes.ts
@@ -0,0 +1,87 @@
+import {UserController} from "./controller/UserController";
+import {ListToDoController} from "./controller/ListTodoController";
+
+export const Routes = [
+{
+    method: "get",
+    route: "/users",
+    controller: UserController,
+    action: "all"
+}, 
+{
+    method: "get",
+    route: "/users/:id",
+    controller: UserController,
+    action: "one"
+}, 
+{
+    method: "post",
+    route: "/users",
+    controller: UserController,
+    action: "save"
+}, 
+{
+    method: "delete",
+    route: "/users/:id",
+    controller: UserController,
+    action: "remove"
+},
+
+{   
+    method: "get",
+    route: "/todos",
+    controller: ListToDoController,
+    action: "all"
+}, {
+    method: "get",
+    route: "/todos/:id",
+    controller: ListToDoController,
+    action: "one"
+}, 
+{
+    method: "post",
+    route: "/todos",
+    controller: ListToDoController,
+    action: "addNew"
+}, 
+{
+    method: "delete",
+    route: "/todos/:id",
+    controller: ListToDoController,
+    action: "remove"
+}, {
+    method: "put",
+    route: "/todos/:id",
+    controller: ListToDoController,
+    action: "markComplete"
+}, {
+    method: "post",
+    route: "/todos/updatetask/:id",
+    controller: ListToDoController,
+    action: "updateTask"
+}, {
+    method: "get",
+    route: "/listall",
+    controller: ListToDoController,
+    action: "listAll"
+},
+{
+    method: "get",
+    route: "/todospersonal",
+    controller: ListToDoController,
+    action: "filterPersonal"
+},
+{
+    method: "get",
+    route: "/todoswork",
+    controller: ListToDoController,
+    action: "filterWork"
+},
+{
+    method: "get",
+    route: "/todosfamily",
+    controller: ListToDoController,
+    action: "filterFamily"
+}
+
+];
