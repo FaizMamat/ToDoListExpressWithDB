a3af05c1a618883fff7b3b2741acf7a21556cd24
FaizMamat <faiz_mamat@dell.com>
2019-09-30 12:15:07 +0800

    Complete

--- src/controller/ListTodoController.ts
+++ src/controller/ListTodoController.ts
@@ -25,8 +25,8 @@ export class ListToDoController {
         return this.todoRepository.save({
             description: request.body.description,
             completed: false,
-            created_at: new Date(),
-            completed_at: new Date("2000-01-01"),
+            create_at: new Date(),
+            complete_at: new Date('2000-01-01'),
             notes: request.body.notes,
             category: request.body.category,
         });
